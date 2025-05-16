01be414ae0daffe430737065028bc943a0040e27
ameda71 <amedasaiteja75@gmail.com>
2025-05-16 17:57:33 +0530

    Update backend.tf

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket  = "my-terraform-bucket-20250516-unique123"  # Must match the bucket created
+    bucket  = "my-terraform-bucket-20250516-unique1234"  # Must match the bucket created
     prefix  = "terraform/state/sonar-vm"
   }
 }
