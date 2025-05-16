bec3a75c3a1fc94f5b4d5315bcc48450f2f57937
ameda71 <amedasaiteja75@gmail.com>
2025-05-16 18:20:54 +0530

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -1,9 +1,23 @@
 provider "google" {
-  project = "symbolic-pipe-457709-n9"                  # Your GCP project ID
-  
+  project = "symbolic-pipe-457709-n9"
   zone    = "europe-west12-b"
 }
 
 resource "google_compute_instance" "sonar" {
-  
+  name         = "sonar"
+  machine_type = "e2-medium"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-stream-9"
+    }
   }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
