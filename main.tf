decac15e76ae2f79cdcc4ff9a1b70cdc70c6595f
ameda71 <amedasaiteja75@gmail.com>
2025-05-16 16:23:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,8 +21,4 @@ resource "google_storage_bucket" "instance_state_file_bucket" {
   
 }
 
-(* resource "google_storage_bucket_object" "empty_folder" {
-  name   = "empty_folder" # folder name should end with '/'
-  content = " Hellosaicharan.txt"            # content is ignored but should be non-empty
-  bucket = google_storage_bucket.mug.name
-} *)
+
