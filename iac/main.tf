df7d3b9b8f51582e8d2e05b0ae96fe3f5b4fb7d5
saicharan <141946357+Saicharan619@users.noreply.github.com>
2025-05-18 00:01:03 +0530

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -1,7 +1,6 @@
 provider "google" {
   project = "saicharan-457910"                  # Your GCP project ID
-  
-//  zone    = "us-east1-b"
+   zone    = "europe-north2-b"
 }
 
 resource "google_compute_instance" "instance-20250505-105229" {
