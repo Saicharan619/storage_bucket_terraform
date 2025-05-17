0b5a2a4281095c48e813a2fe5de6956aa83beab5
saicharan <141946357+Saicharan619@users.noreply.github.com>
2025-05-18 00:09:08 +0530

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -1,8 +1,8 @@
 provider "google" {
-  project = "saicharan-457910"                  # Your GCP project ID
-   zone    = "europe-north2-b"
+  project = "saicharan-457910"
+  zone    = "europe-north2-b"
 }
 
 resource "google_compute_instance" "instance-20250505-105229" {
-  
-  }
+  # Empty block for import — do NOT fill this yet
+}
