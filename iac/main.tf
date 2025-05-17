97d6ed47a96fc94f9ac4d164e8eb242269c86f20
saicharan <141946357+Saicharan619@users.noreply.github.com>
2025-05-18 00:32:20 +0530

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -1,18 +1,8 @@
+provider "google" {
+  project = "saicharan-457910"
+  zone    = "europe-north2-b"
+}
+
 resource "google_compute_instance" "instance-20250505-105229" {
-  name         = "instance-20250505-105229"
-  machine_type = "e2-medium"
-
-  boot_disk {
-    initialize_params {
-      image = "centos-stream-9-v20250415"
-      size  = 20
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {}
-  }
-
-  zone = "europe-north2-b"
+  # Empty resource block — required for import only
 }
